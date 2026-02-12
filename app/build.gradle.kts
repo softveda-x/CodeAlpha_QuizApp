e2b3365b2224e5d0e04e368d15e1f213c513790f
softveda-x <merajalam8294@gmail.>
2026-02-12 22:22:48 +0530

    Updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,10 +8,11 @@ android {
 
     defaultConfig {
         applicationId = "com.example.flashcardquizapp"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
+        multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
